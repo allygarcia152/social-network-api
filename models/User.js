3a8ad8f0bc347c5d5cfedabd544a3f616e3ecc4a
AllyGarcia <allygarcia152@gmail.com>
2022-06-12 15:54:29 -0500

    add User model in modoels folder

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const validateEmail = (email) => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [
+        validateEmail,
+        "Please use a valid email address."
+      ],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+// get total count of friends and replies on retrieval
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+});
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+// export the User model
+module.exports = User;
