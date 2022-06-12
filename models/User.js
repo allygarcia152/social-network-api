0fab41a6558bcfc839e181363122cace918c7cbc
AllyGarcia <allygarcia152@gmail.com>
2022-06-12 16:11:41 -0500

    add Thought model with thought and reaction schema as well as imported all models into models/index.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
 
 const validateEmail = (email) => {
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
